0ca790e0a7580e759e5f5aaa648966fc9f474830
Seb Dangerfield <seb.dangerfield@capablue.com>
2015-03-26 17:06:52 +0000

    Ensuring the client mount class compiles

--- spec/defines/client_spec.rb
+++ spec/defines/client_spec.rb
@@ -5,6 +5,8 @@ describe 'nfs::client::mount', :type => :define do
   let(:facts) { { :operatingsystem => 'ubuntu' } }
   let(:params) {{ :server => 'nfs.int.net', :share => '/srv/share' } }
   it do
+    should compile
     should contain_class('nfs::client')
+    should contain_mount('shared /srv/test by nfs.int.net')
   end
 end
