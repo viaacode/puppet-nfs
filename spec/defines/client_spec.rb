d171b5970138b5dc6c95ef95dc6b8fe4fbcf4403
Seb Dangerfield <seb.dangerfield@capablue.com>
2015-03-26 17:54:01 +0000

    Recreated bug with naming of nfs exports

--- spec/defines/client_spec.rb
+++ spec/defines/client_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 
 describe 'nfs::client::mount', :type => :define do
-  let(:title) { '/srv/test' }
+  let(:title) { 'shared /srv/test by nfs.int.net' }
   let(:facts) { { :operatingsystem => 'ubuntu' } }
   let(:params) {{ :server => 'nfs.int.net', :share => '/srv/share' } }
   it do
     should compile
     should contain_class('nfs::client')
-    should contain_mount('shared /srv/test by nfs.int.net')
   end
 end
