752a5bd45d23a77d0bad9e5c03173197a107be46
Seb Dangerfield <seb.dangerfield@capablue.com>
2015-03-27 09:12:54 +0000

    Updated client mount define spec to be more like real exported resource

--- spec/defines/client_spec.rb
+++ spec/defines/client_spec.rb
@@ -1,11 +1,27 @@
 require 'spec_helper'
 
 describe 'nfs::client::mount', :type => :define do
-  let(:title) { 'shared /srv/test by nfs.int.net' }
-  let(:facts) { { :operatingsystem => 'ubuntu' } }
-  let(:params) {{ :server => 'nfs.int.net', :share => '/srv/share' } }
-  it do
-    should compile
-    should contain_class('nfs::client')
+  context "Mount created by exported resource" do
+    let(:title) { 'shared /srv/test by nfs.int.net' }
+    let(:facts) { { :operatingsystem => 'ubuntu' } }
+    let(:params) {{ 
+      :server => 'nfs.int.net',
+      :share  => '/srv/share',
+      :mount  => Undef.new
+    }}
+    it do
+      should compile
+      should contain_class('nfs::client')
+    end
+  end
+
+  context "Mount manually set" do
+    let(:title) { '/srv/test' }
+    let(:facts) { { :operatingsystem => 'ubuntu' } }
+    let(:params) {{ :server => 'nfs.int.net', :share => '/srv/share' } }
+    it do
+      should compile
+      should contain_class('nfs::client')
+    end
   end
 end
