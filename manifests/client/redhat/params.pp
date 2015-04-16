1804ff5df9a8b89c6d018e691fce7c1dc91f497e
Davem <dave.mincemoyer@slalom.com>
2015-04-16 10:22:18 -0500

    Add support for Amazon linux

--- manifests/client/redhat/params.pp
+++ manifests/client/redhat/params.pp
@@ -17,6 +17,10 @@ class nfs::client::redhat::params {
     /^\d{2,}/: {
       $osmajor = 7
     }
+    # Amazon linux operatingsystemrelease is verbose: 3.10.35-43.137.amzn1.x86_64
+    /^3\.(\d|-|\.)+(amzn){1}/: {
+      $osmajor = 6
+    }
     default:{
       fail("Operatingsystemrelease ${::operatingsystemrelease} not supported")
     }
