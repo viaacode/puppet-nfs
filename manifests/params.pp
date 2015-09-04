12f9a0c4e23e1a1ab8ecedb43033c3b99e3fdedc
Julien Pivotto <roidelapluie@inuits.eu>
2015-09-04 17:15:43 +0200

    Fix Typos

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,7 +6,7 @@ class nfs::params (
   $nfs_v4_idmap_domain        = $::domain
 ) {
 
-  # Somehow the ::osfamliy fact doesnt exist on some oled systems
+  # Somehow the ::osfamily fact doesnt exist on some systems
 
   case $::operatingsystem {
     'centos', 'redhat', 'scientific', 'fedora', 'SLC', 'OracleLinux', 'Amazon' : { $osfamily = 'redhat' }
