b55d4d7838d3d4a48242dc67d3913bb3c3b9b545
Daniel Sung <Daniel.sung@me.com>
2016-01-07 17:20:42 +0000

    Enable the idmapd service so that it starts on boot
    If the idmapd service is disabled, make sure it is enabled on Ubuntu if
    NFSv4 is configured. Also make sure that it is disabled if NFSv4 is not
    configured otherwise it would start on boot.

--- manifests/client/ubuntu/service.pp
+++ manifests/client/ubuntu/service.pp
@@ -9,9 +9,13 @@ class nfs::client::ubuntu::service {
   if $nfs::client::ubuntu::nfs_v4 {
     service { 'idmapd':
       ensure    => running,
+      enable    => true,
       subscribe => Augeas['/etc/idmapd.conf', '/etc/default/nfs-common'],
     }
   } else {
-    service { 'idmapd': ensure => stopped, }
+    service { 'idmapd':
+      ensure => stopped,
+      enable => false,
+    }
   }
 }
