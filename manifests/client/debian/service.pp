0c275c76ba2855a68b5eda39bb4fb72541b3c8a2
Daniel Sung <Daniel.sung@me.com>
2016-01-08 09:27:11 +0000

    Enable idmapd service resource on Debian when NFSv4 configured

--- manifests/client/debian/service.pp
+++ manifests/client/debian/service.pp
@@ -9,6 +9,7 @@ class nfs::client::debian::service {
   if $nfs::client::debian::nfs_v4 {
     service { 'idmapd':
       ensure    => running,
+      enable    => true,
       name      => 'nfs-common',
       subscribe => Augeas['/etc/idmapd.conf', '/etc/default/nfs-common'],
     }
