36e0f53a24614720c56ca2be64c422525bf3301e
O'Shaughnessy Evans <shaug@act-on.net>
2016-01-27 10:50:56 -0800

    teach the module to recognize newer releases of Amazon Linux, which use YYYY.MM format for $::operatingsystemrelease

--- manifests/client/redhat/params.pp
+++ manifests/client/redhat/params.pp
@@ -13,13 +13,17 @@ class nfs::client::redhat::params {
     /^7\.\d+/: {
       $osmajor = 7
     }
-    # TODO: workaround for Fedora
-    /^\d{2,}/: {
-      $osmajor = 7
+    # Newer Amazon Linux releases use YYYY.MM (e.g. "2014.09" or "2015.03")
+    /^\d{4}\.\d{2}$/: {
+      $osmajor = 6
     }
     # Amazon linux operatingsystemrelease is verbose: 3.10.35-43.137.amzn1.x86_64
     /^[34]\.(\d|-|\.)+(amzn){1}/: {
       $osmajor = 6
+    }
+    # TODO: workaround for Fedora
+    /^\d{2,}/: {
+      $osmajor = 7
     }
     default:{
       fail("Operatingsystemrelease ${::operatingsystemrelease} not supported")
