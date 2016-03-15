d4dd0ca5635495ceec3b2fecdb8ed8c08bdfea69
Tina <cochet.tina@telenet.be>
2016-03-15 15:50:20 +0100

    disable mkdir

--- manifests/mkdir.pp
+++ manifests/mkdir.pp
@@ -1,21 +1,21 @@
 # Nasty ass hax to allow several levels of directories
 define nfs::mkdir (
-  $owner = 'root',
-  $group = 'root',
-  $perm  = '0755'
+  #$owner = 'root',
+  #$group = 'root',
+  #$perm  = '0755'
 ) {
-  exec { "mkdir_recurse_${name}":
-    path    => ['/bin', '/usr/bin'],
-    command => "mkdir -p ${name}",
-    unless  => "test -d ${name}",
-  }
+  #exec { "mkdir_recurse_${name}":
+  #  path    => ['/bin', '/usr/bin'],
+  #  command => "mkdir -p ${name}",
+  #  unless  => "test -d ${name}",
+  #}
 
-  file { $name:
-    ensure  => directory,
-    require => Exec["mkdir_recurse_${name}"],
-    mode    => $perm,
-    owner   => $owner,
-    group   => $group,
-  }
+  #file { $name:
+  #  ensure  => directory,
+  #  require => Exec["mkdir_recurse_${name}"],
+  #  mode    => $perm,
+  #  owner   => $owner,
+  #  group   => $group,
+  #}
 
 }
